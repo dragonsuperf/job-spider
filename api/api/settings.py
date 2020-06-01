85e52879386a1ed0114ec251d73f06db49b791d4
dragonsuperf <dragonsuperf@gmail.com>
2020-06-01 19:46:39 +0900

    db 세팅 수정

--- api/api/settings.py
+++ api/api/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': os.environ.get('SQL_USER', 'user'),
-        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
-        'HOST': os.environ.get('SQL_HOST', 'localhost'),
-        'PORT': os.environ.get('SQL_PORT', '5432'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dsf',
+        'USER': 'dsf',
+        'PASSWORD': 'elql@19',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 

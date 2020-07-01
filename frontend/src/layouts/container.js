0a20a4e6cb0462b5d5a67c5ae9d55ed5ec854fe5
Nomore12 <nightwing@naver.com>
2020-07-01 19:18:13 +0900

    헤더 메뉴 정리

--- frontend/src/layouts/container.js
+++ frontend/src/layouts/container.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom'
 import { HeaderNav } from './headerNav';
-// import { Content } from './content';
 import { Footer } from './footer';
 import { RecruitArea, OfferArea, CompanyPage, ResumePage, ApplicationArea } from '../pages/index'
 
@@ -12,9 +11,6 @@ const Container = () => {
             <div className="contentArea">
                 <Route path="/" exact={true} component={RecruitArea}/>
                 <Route path="/company" component={CompanyPage}/>
-                {/* <Route path="/offer" component={OfferArea}/>
-                <Route path="/application" component={ApplicationArea}/>
-                <Route path="/resume" component={ResumePage}/> */}
             </div>
             <Footer/>
         </Fragment>
--- frontend/src/layouts/headerNav.js
+++ frontend/src/layouts/headerNav.js
@@ -1,49 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const HeaderNav = () => {
-    return (
-        <header className='headerNav'>
-            <div className="nav_title">
-                <img className="site_logo" 
-                    src="/images/site/main_logo.png" 
-                    width="50rem" 
-                    height="50rem"/>
-                <h1 className="site_title">Job-Spider</h1>
-            </div>
-            <nav className="nav_center">
-                <ul className="nav_menus">
-                    <li className="menu_item">
-                        <Link to="/" className="menu_item__link">
-                            채용 공고
-                        </Link>
-                    </li>
-                    <li className="menu_item">
-                        <Link to="company" className="menu_item__link">
-                            회사 목록
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            {/* <div className="nav_user">
-                <ul className="nav_links">
-                    <li className="nav_link">
-                        <Link className="nav_link__href" to="resume">
-                            <i className="nav_icon fas fa-file-alt" title="이력서"></i>
-                        </Link>
-                        <p className="icon_tooltip">이력서</p>
-                    </li>
-                    <li className="nav_link">
-                        <i className="nav_icon fas fa-bell" title="알림"></i>
-                        <p className="icon_tooltip">알림</p>
-                    </li>
-                    <li className="nav_link">
-                        <i className="nav_icon fas fa-id-badge" title="마이페이지"></i>
-                        <p className="icon_tooltip">마이페이지</p>
-                    </li>
-                </ul>
-                </div> */}
-            <img className="mobile_menu__icon" src="/images/menus/burger.png"/>
-        </header>
-    )
-}
+  return (
+    <header className="header_nav">
+      <div className="nav_title">
+        <img className="site_logo" src="/images/site/temp_logo.png" />
+      </div>
+      <nav className="nav_center">
+        <ul className="nav_menus">
+          <li className="menu_item">
+            <Link to="/" className="menu_item__link">
+              채용 공고
+            </Link>
+          </li>
+          <li className="menu_item">
+            <Link to="company" className="menu_item__link">
+              회사 목록
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <nav className="mobile_nav">
+        <div className="hidden_title">
+          <img
+            className="mobile_logo"
+            src="/images/site/temp_mobile_logo.png"
+          />
+        </div>
+        <div className="mobile_menu">
+          <img className="mobile_menu__icon" src="/images/menus/burger.png" />
+        </div>
+      </nav>
+    </header>
+  );
+};
